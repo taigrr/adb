0dddd8ca0911c29195ee861793c61782b9e52bd0
Tai Groot <tai@taigrr.com>
2022-08-02 20:16:09 -0700

    add more unit tests to parseDevices

--- adb_test.go
+++ adb_test.go
@@ -16,6 +16,27 @@ func Test_parseDevices(t *testing.T) {
 		wantErr bool
 	}{
 		{
+			name: "no devices", args: args{stdout: `List of devices attached`},
+			wantErr: false,
+			want:    []Device{},
+		},
+		{
+			name: "1 auth dev", args: args{stdout: `List of devices attached
+19291FDEE0023W  device`},
+			wantErr: false,
+			want: []Device{
+				{IsAuthorized: true, SerialNo: "19291FDEE0023W"},
+			},
+		},
+		{
+			name: "1 unauth dev", args: args{stdout: `List of devices attached
+HT75R0202681    unauthorized`},
+			wantErr: false,
+			want: []Device{
+				{IsAuthorized: false, SerialNo: "HT75R0202681"},
+			},
+		},
+		{
 			name: "2 auth 1 unauth", args: args{stdout: `List of devices attached
 19291FDEE0023W  device
 9B061FFBA00BC9  device
